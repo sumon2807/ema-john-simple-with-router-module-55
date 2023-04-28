8486b66c0ec4f8194bd82f5edde0a8daa2ae14da
sumon2807 <sharifulbd911@gmail.com>
2023-04-28 22:55:17 +0600

    firebase authprovider setup compleate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Header = () => {
+    const {user}=useContext(AuthContext);
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -13,6 +15,9 @@ const Header = () => {
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">SignUp</Link>
+                {
+                    user && <span>{user.displayName}</span>
+                }
             </div>
         </nav>
     );
